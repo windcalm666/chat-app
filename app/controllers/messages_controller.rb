dfe54e1b7bbff2f7082abf4be56d0b5cd91694ad
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-02-23 15:53:43 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,7 +11,9 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
+    end
   end
 
   private
